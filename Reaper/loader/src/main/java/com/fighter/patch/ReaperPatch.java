1127c6a4414a5fea37b4c9be6c309d063242b1c8
wanghaiteng <wanghaiteng@qiku.com>
2017-05-05 18:14:09 +0800

    Add wrapper package and fixed unknown type value of ReaperPatch
    Signed-off-by: wanghaiteng <wanghaiteng@qiku.com>

--- Reaper/loader/src/main/java/com/fighter/patch/ReaperPatch.java
+++ Reaper/loader/src/main/java/com/fighter/patch/ReaperPatch.java
@@ -13,18 +13,25 @@ import dalvik.system.PathClassLoader;
  * Created by wxthon on 5/5/17.
  */
 
+/**
+ * Reaper patch object
+ * You can find reaper core class by this patch through ReaperClassLoader
+ * This patch can decrypt .rr file
+ */
 public class ReaperPatch {
 
     // Patch Types
-    public static final int TYPE_UNKNOWN    = 0x01;
+    public static final int TYPE_UNKNOWN    = 0x00;
     public static final int TYPE_DEX        = 0x01;
     public static final int TYPE_APK        = 0x02;
     public static final int TYPE_REAPER     = 0x03;
 
+    // private final members
     private final String PATCH_DIR = "patch";
     private final String PATCH_OPT_DIR = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + PATCH_DIR + "/opt";
     private final String PATCH_LIB_DIR = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + PATCH_DIR + "/libs";
 
+    // private members
     private File mFile;
     private ClassLoader mLoader;
     private int mType;
--- Reaper/reaper/src/main/java/com/fighter/reaper/BumpVersion.java
+++ Reaper/reaper/src/main/java/com/fighter/reaper/BumpVersion.java
@@ -4,8 +4,16 @@ package com.fighter.reaper;
  * Created by wxthon on 5/5/17.
  */
 
+
+/**
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * ! Don't modified variables, just bump version value, it's dangerous !
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
+
 public class BumpVersion {
 
+    // Must be final constant
     public static final int RELEASE = 1;
     public static final int SECOND = 0;
     public static final int REVISION = 0;
