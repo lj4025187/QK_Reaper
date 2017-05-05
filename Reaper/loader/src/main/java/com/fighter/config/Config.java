4f7e3ca6ff02f007666da655d4e9ca38f7c34dbd
zhangjigang <zhangjigang@qiku.com>
2017-05-05 21:35:16 +0800

    add proguard, publish dex , publish jar
    Signed-off-by: zhangjigang <zhangjigang@qiku.com>

---
+++ Reaper/loader/src/main/java/com/fighter/config/Config.java
@@ -0,0 +1,18 @@
+package com.fighter.config;
+
+import com.qiku.proguard.annotations.KeepAll;
+
+/**
+ * Configurations about the sdk
+ *
+ * Created by zhangjg on 17-5-5.
+ */
+
+@KeepAll
+public class Config {
+
+    /**
+     * The sdk version
+     */
+    public static final String VERSION = "1.0.0";
+}
