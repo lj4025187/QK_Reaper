8ba990fad1c6a2bf8ab642dc3304a71096f067ab
Matti <Matti@MattideMacBook-Pro.local>
2017-05-12 21:30:14 +0800

    Add download for higher version reaper.
    Signed-off-by: Matti <Matti@MattideMacBook-Pro.local>

--- Reaper/reaper/src/main/java/com/fighter/download/ReaperDownload.java
+++ Reaper/reaper/src/main/java/com/fighter/download/ReaperDownload.java
@@ -1,9 +1,361 @@
 package com.fighter.download;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
+
+import com.fighter.common.utils.ReaperLog;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 /**
  * Created by huayang on 17-5-10.
  */
 
 public class ReaperDownload {
+    private static final String TAG = ReaperDownload.class.getSimpleName();
+    private static final boolean DEBUG_DOWNLOAD = true;
+
+    private static final int REAPER_VERSION_CHECK_NEW_VERSION = 1;
+    private static final int REAPER_VERSION_CHECK_SAME_VERSION = 0;
+    private static final int REAPER_VERSION_CHECK_FAILED = -1;
+
+    private static final int COMPARE_SERVER_HIGHER = 1;
+    private static final int COMPARE_CURRENT_HIGHER = -1;
+    private static final int COMPARE_EQUALS = 0;
+    private static final int COMPARE_FAILED = -10;
+
+    /**
+     * Check higher version
+     * @return
+     * 1 = check success and has higher version
+     * 0 = check success and dont have higher version
+     * -1 = check failed
+     */
+    public static int doQuery(String version, String sdkAbsPath) {
+        if (TextUtils.isEmpty(version) || !isValidVersion(version))
+            return REAPER_VERSION_CHECK_FAILED;
+
+        String serverVersion = queryVersion(sdkAbsPath);
+        if (!isValidVersion(serverVersion)) {
+            if (DEBUG_DOWNLOAD) {
+                ReaperLog.e(TAG, "query a bad version. : " + serverVersion);
+            }
+            return REAPER_VERSION_CHECK_FAILED;
+        }
+
+        int compared = compareVersion(version, serverVersion);
+        ReaperLog.e(TAG, "compared : " + compared);
+        if (compared != COMPARE_SERVER_HIGHER) {
+            if (compared == COMPARE_FAILED) {
+                return REAPER_VERSION_CHECK_FAILED;
+            }
+            return REAPER_VERSION_CHECK_SAME_VERSION;
+        }
+
+        //server has a higher version
+        boolean success = downloadHigherVersionReaper();
+        if (success) {
+            return REAPER_VERSION_CHECK_NEW_VERSION;
+        }
+
+        return REAPER_VERSION_CHECK_FAILED;
+    }
+
+    private static String queryVersion(String sdkAbsPath) {
+        String url = "https://kyfw.12306.cn/otn/";
+        AssetManager assetManager= null;
+        try {
+            assetManager = AssetManager.class.newInstance();
+            Method addPathMethod = AssetManager.class.getDeclaredMethod("addAssetPath", String.class);
+            if (addPathMethod == null)
+                return null;
+            addPathMethod.setAccessible(true);
+            addPathMethod.invoke(assetManager, sdkAbsPath);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        if (assetManager == null) {
+            ReaperLog.e(TAG, "assetManager : " + assetManager);
+            return null;
+        }
+
+        HttpsUtil httpsUtil = new HttpsUtil(assetManager);
+        try {
+            httpsUtil.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        InputStream is = null;
+        try {
+            is = assetManager.open("srca.cer");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (is == null)
+            return null;
+        OkHttpClient client = setCertificates(is);
+        if (is != null) {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ReaperLog.e(TAG, "client : " + client);
+        if (client == null)
+            return null;
+
+        /*Request.Builder builder = new Request.Builder();
+        Request request = builder.url(url).build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response == null || !response.isSuccessful()) {
+                ReaperLog.e(TAG, "bad response .");
+                return null;
+            }
+            ResponseBody responseBody= response.body();
+            if (responseBody == null) {
+                ReaperLog.e(TAG, "responseBody == null");
+                return null;
+            }
+            ReaperLog.e(TAG, "body : " + responseBody.string());
+//            InputStream is = responseBody.byteStream();
+//            if (is == null) {
+//                ReaperLog.e(TAG, "body byteStream == null.");
+//                return null;
+//            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            ReaperLog.e(TAG, "queryVersion : " + e.getMessage());
+        }*/
+        return "1.0.0";
+    }
+
+    private static boolean downloadHigherVersionReaper() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param currentVersion
+     * @param serverVersion
+     * @return
+     * 1, when serverVersion > currentVersion
+     * 0, when serverVersion = currentVersion
+     * -1, when serverVersion < currentVersion
+     * -10, error
+     */
+    private static int compareVersion(String currentVersion, String serverVersion) {
+        String[] currentSplitStr = splitVersionStr(currentVersion);
+        String[] serverSplitStr = splitVersionStr(serverVersion);
+        if (currentSplitStr == null || serverSplitStr == null)
+            return COMPARE_FAILED;
+
+        try {
+            int currentRelease = Integer.valueOf(currentSplitStr[0]);
+            int currentSecond = Integer.valueOf(currentSplitStr[1]);
+            int currentRevision = Integer.valueOf(currentSplitStr[2]);
+
+            int serverRelease = Integer.valueOf(serverSplitStr[0]);
+            int serverSecond = Integer.valueOf(serverSplitStr[1]);
+            int serverRevision = Integer.valueOf(serverSplitStr[2]);
+
+            if (DEBUG_DOWNLOAD) {
+                ReaperLog.e(TAG, "server : " + getSplitString(serverSplitStr));
+                ReaperLog.e(TAG, "current : " + getSplitString(currentSplitStr));
+            }
+            if (serverRelease > currentRelease) {
+                return COMPARE_SERVER_HIGHER;
+            } else if (serverRelease < currentRelease){
+                return COMPARE_CURRENT_HIGHER;
+            }
+
+            //serverRelease == currentRelease
+            if (serverSecond > currentSecond) {
+                return COMPARE_SERVER_HIGHER;
+            } else if (serverSecond < currentSecond) {
+                return COMPARE_CURRENT_HIGHER;
+            }
+
+            //serverSecond == currentSecond
+            if (serverRevision > currentRevision) {
+                return COMPARE_SERVER_HIGHER;
+            } else if (serverRevision < currentRevision) {
+                return COMPARE_CURRENT_HIGHER;
+            }
+
+            //serverRevision == currentRevision
+            if (serverSplitStr.length != 4 || currentSplitStr.length != 4) {
+                return COMPARE_EQUALS;
+            }
+
+            //both has suffix
+            String alpha = "-alpha";
+            String beta = "-beta";
+            String stable = "-stable";
+            if (TextUtils.isEmpty(serverSplitStr[3])
+                    || TextUtils.isEmpty(currentSplitStr[3])
+                    || TextUtils.equals(serverSplitStr[3], currentSplitStr[3])) {
+                return COMPARE_EQUALS;
+            }
+            boolean serverAlpha = TextUtils.equals(serverSplitStr[3], alpha);
+            boolean serverBeta = TextUtils.equals(serverSplitStr[3], beta);
+            boolean serverStable = TextUtils.equals(serverSplitStr[3], stable);
+            boolean currentAlpha = TextUtils.equals(currentSplitStr[3], alpha);
+            boolean currentBeta = TextUtils.equals(currentSplitStr[3], beta);
+            boolean currentStable = TextUtils.equals(currentSplitStr[3], stable);
+            if ((serverStable && (currentBeta || currentAlpha)) ||
+                    (serverBeta && currentAlpha)) {
+                return COMPARE_SERVER_HIGHER;
+            } else if ((currentStable && (serverBeta || serverAlpha)) ||
+                    (currentBeta && serverAlpha)) {
+                return COMPARE_CURRENT_HIGHER;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return COMPARE_FAILED;
+        }
+        return COMPARE_FAILED;
+    }
+
+    private static boolean isValidVersion(String version) {
+        String[] splitVerStr = splitVersionStr(version);
+        if (splitVerStr == null)
+            return false;
+        try {
+            Integer.valueOf(splitVerStr[0]);
+            Integer.valueOf(splitVerStr[1]);
+            Integer.valueOf(splitVerStr[2]);
+            return true;
+        } catch (Exception e){
+            if (DEBUG_DOWNLOAD) {
+                ReaperLog.e(TAG, "err msg : " + e.getMessage());
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static String[] splitVersionStr(String version) {
+        if (TextUtils.isEmpty(version))
+            return null;
+        String[] split = version.split("\\.");
+        if (split.length < 3)
+            return null;
+        if (split[2].contains("-")) {
+            String[] last = split[2].split("-");
+            if (last.length != 2) {
+                if (DEBUG_DOWNLOAD) {
+                    ReaperLog.e(TAG, "splitVersionStr error !");
+                }
+                return null;
+            }
+            return new String[] {split[0], split[1], last[0], "-" + last[1]};
+        }
+
+        return split;
+    }
+
+    private static String getSplitString(String[] split) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : split) {
+            sb.append(s).append(".");
+        }
+        return sb.toString();
+    }
+
+    private static OkHttpClient setCertificates(InputStream... certificates) {
+        try {
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null);
+            int index = 0;
+            for (InputStream certificate : certificates) {
+                String certificateAlias = Integer.toString(index++);
+                keyStore.setCertificateEntry(certificateAlias, certificateFactory.generateCertificate(certificate));
+
+                try {
+                    if (certificate != null)
+                        certificate.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            TrustManagerFactory trustManagerFactory =
+                    TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+
+            trustManagerFactory.init(keyStore);
+            sslContext.init
+                    (
+                            null,
+                            trustManagerFactory.getTrustManagers(),
+                            new SecureRandom()
+                    );
+            X509TrustManager trustManager = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            };
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .sslSocketFactory(sslContext.getSocketFactory(), trustManager)
+                    .build();
+            return client;
+        } catch (Exception e) {
+            e.printStackTrace();
+            ReaperLog.e(TAG, "cert err : " + e.getMessage());
+        }
+        return null;
+    }
+
+
 
 }
