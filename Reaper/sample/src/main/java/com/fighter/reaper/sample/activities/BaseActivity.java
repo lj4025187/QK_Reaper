b6bef961a933b882a0668ee699e0571d9190ad87
liujia2 <liujia2@qiku.com>
2017-06-21 10:57:42 +0800

    Add permission request logic in sample
    Signed-off-by: liujia2 <liujia2@qiku.com>

--- Reaper/sample/src/main/java/com/fighter/reaper/sample/activities/BaseActivity.java
+++ Reaper/sample/src/main/java/com/fighter/reaper/sample/activities/BaseActivity.java
@@ -4,14 +4,18 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 
@@ -19,6 +23,7 @@ import com.fighter.loader.ReaperApi;
 import com.fighter.reaper.sample.SampleApp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Administrator on 2017/5/19.
@@ -33,14 +38,10 @@ public class BaseActivity extends FragmentActivity implements Handler.Callback {
     private final static String[] REQUESTED_PERMISSIONS = {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.READ_PHONE_STATE,
-            Manifest.permission.INTERNET,
-            Manifest.permission.ACCESS_WIFI_STATE,
-            Manifest.permission.ACCESS_NETWORK_STATE,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_SETTINGS,
-            Manifest.permission.CHANGE_WIFI_STATE
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.WRITE_SETTINGS
     };
 
     private final static int REQUEST_CODE = 5555;
@@ -68,19 +69,33 @@ public class BaseActivity extends FragmentActivity implements Handler.Callback {
         }
         if(noGrantedPermission.isEmpty()) return;
         String[] needRequests = noGrantedPermission.toArray(new String[noGrantedPermission.size()]);
-        requestPermissions(needRequests, REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, needRequests, REQUEST_CODE);
     }
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode != REQUEST_CODE || permissions.length == 0) return;
-        int length = permissions.length;
-//        for (int i = 0; i < length; i++) {
-//            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-//                checkPermission();
-//            }
-//        }
+        if(Arrays.asList(permissions).contains(Manifest.permission.WRITE_SETTINGS))
+                checkWriteSettings();
+        if(Arrays.asList(grantResults).contains(PackageManager.PERMISSION_DENIED))
+                startPermissionSettings();
+    }
+
+    private void startPermissionSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void checkWriteSettings() {
+        if(!Settings.System.canWrite(this)){
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivityForResult(intent, REQUEST_CODE);
+        }
     }
 
     @Override
