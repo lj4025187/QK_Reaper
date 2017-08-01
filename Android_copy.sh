a507126cf339db595e8960ebc7d84becc7ae9c64
liujia2 <liujia2@qiku.com>
2017-08-01 16:05:36 +0800

    mv Andorid_copy.sh to bat file
    Change-Id: Id1eb07fbaf0046d1b6b20ffd1f87be04e6e485d1
    Signed-off-by: liujia2 <liujia2@qiku.com>

--- Android_copy.sh
+++
@@ -1,8 +0,0 @@
-cd QK_Reaper;
-rm -rf bin/;
-gradle clean;gradle assembleDebug;
-cd ..;
-mkdir bin;
-cp QK_Reaper/bin/reaper.aar bin/;
-cp QK_Reaper/bin/reaper.rr bin/;
-cp QK_Reaper/bin/sample.apk bin/;
