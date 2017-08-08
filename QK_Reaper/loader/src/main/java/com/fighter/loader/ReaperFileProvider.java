94960454ef903dcfcc721901d5926fdd2bbc0c1a
liujia2 <liujia2@qiku.com>
2017-08-08 19:02:07 +0800

    Modify the FileProvider to ReaperFileProvider
    Change-Id: I703f7b6aebc88e66429e110c4ab19474bbbe4390
    Signed-off-by: liujia2 <liujia2@qiku.com>

---
+++ QK_Reaper/loader/src/main/java/com/fighter/loader/ReaperFileProvider.java
@@ -0,0 +1,13 @@
+package com.fighter.loader;
+
+import android.support.v4.content.FileProvider;
+
+import com.qiku.proguard.annotations.NoProguard;
+
+/**
+ * Created by jia on 8/8/17.
+ */
+@NoProguard
+public class ReaperFileProvider extends FileProvider{
+
+}
