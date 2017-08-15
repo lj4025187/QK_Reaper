9a0f7171a718623d4fa894a1dc619ad0bacd94a0
lichen <lichen1-iri@360.cn>
2017-08-15 18:37:08 +0800

    fix hook reaper provider err
    Signed-off-by: lichen <lichen1-iri@360.cn>

--- QK_Reaper/reaper/src/main/java/com/fighter/hook/ReaperHookProvider.java
+++ QK_Reaper/reaper/src/main/java/com/fighter/hook/ReaperHookProvider.java
@@ -1,10 +1,15 @@
 package com.fighter.hook;
 
+import android.content.ContentProvider;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
 import android.content.Context;
 
 import com.fighter.common.utils.ReaperLog;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by lichen on 17-8-15.
@@ -18,20 +23,53 @@ public class ReaperHookProvider {
         if (context == null)
             return;
         ClassLoader contextClassLoader = context.getClassLoader();
+        ReaperLog.i(TAG, "context class loader = " + contextClassLoader);
 
         try {
             Class<?> reaperProxyProvider = Class.forName("com.fighter.loader.ReaperProxyProvider", true, contextClassLoader);
+            for (Field field : reaperProxyProvider.getDeclaredFields()) {
+                ReaperLog.i(TAG, "name ===>" + field.getName());
+            }
             Field proxyProvider  = reaperProxyProvider.getDeclaredField("providerProxy");
             proxyProvider.setAccessible(true);
-            proxyProvider.set(reaperProxyProvider.newInstance(), new ProxyProvider());
+            proxyProvider.set(getContentProvider(context), new ProxyProvider());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private static ContentProvider getContentProvider(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        String auth = context.getPackageName() + ".reaper.provider.ReaperProxyProvider";
+
+        ContentProviderClient client = resolver.acquireContentProviderClient(auth);
+        Class<?> clientClz = ContentProviderClient.class;
+        Class<?> contentProviderClz = ContentProvider.class;
+        ContentProvider ret;
+        try {
+            Class IContentProviderClass = Class.forName("android.content.IContentProvider");
+            Field IContentProviderField = clientClz.getDeclaredField("mContentProvider");
+            IContentProviderField.setAccessible(true);
+            Object IContentProvider = IContentProviderField.get(client);
+            Method getProviderMethod = contentProviderClz.getDeclaredMethod("coerceToLocalContentProvider", IContentProviderClass);
+            getProviderMethod.setAccessible(true);
+            ret = (ContentProvider) getProviderMethod.invoke(null, IContentProvider);
+            return ret;
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
