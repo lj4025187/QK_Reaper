8dd090cc9c3725f8840e18220d2cf6a47b018bb6
liujia2 <liujia2@qiku.com>
2017-08-18 14:07:12 +0800

    add simple debug install and release install shell
    Change-Id: I32848aa9d0ddfa6423b0dca043ad55d09b85ada4
    Signed-off-by: liujia2 <liujia2@qiku.com>

---
+++ QK_Reaper/debug_install.sh
@@ -0,0 +1,34 @@
+echo ===============================================
+echo ===================ReaperSDK===================
+echo       ..######..########..##....##
+echo       .##....##.##.....##.##...##.
+echo       .##.......##.....##.##..##..
+echo       ..######..##.....##.#####...
+echo       .......##.##.....##.##..##..
+echo       .##....##.##.....##.##...##.
+echo       ..######..########..##....##
+echo ==================ReaperSDK====================
+echo ===============================================
+
+
+cd loader;
+gradle clean;gradle assembleRelease;
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> loader release generated >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+cd ..;
+cd reaper;
+gradle clean;gradle assembleDebug;
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> reaper debug generated >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+cd ..;
+cp bin/reaper.aar sample/libs;
+cp bin/reaper.rr sample/src/main/assets/;
+cd sample;
+gradle clean;gradle assembleDebug;
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> sample debug generated >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+adb root;
+sleep 2
+adb remount;
+sleep 2
+java -jar ../tools/signapk.jar ../tools/security/platform.x509.pem ../tools/security/platform.pk8 build/outputs/apk/sample-debug.apk build/outputs/apk/sample-debug-signed.apk
+echo ">>>> prepare install debug signed sample apk >>>>"
+adb install -r build/outputs/apk/sample-debug-signed.apk
+cd ..
---
+++ QK_Reaper/release_install.sh
@@ -0,0 +1,33 @@
+echo ===============================================
+echo ===================ReaperSDK===================
+echo       ..######..########..##....##
+echo       .##....##.##.....##.##...##.
+echo       .##.......##.....##.##..##..
+echo       ..######..##.....##.#####...
+echo       .......##.##.....##.##..##..
+echo       .##....##.##.....##.##...##.
+echo       ..######..########..##....##
+echo ==================ReaperSDK====================
+echo ===============================================
+
+cd loader;
+gradle clean;gradle assembleRelease;
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> loader release generated >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+cd ..;
+cd reaper;
+gradle clean;gradle assembleRelease;
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> reaper release generated >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+cd ..;
+cp bin/reaper.aar sample/libs;
+cp bin/reaper.rr sample/src/main/assets/;
+cd sample;
+gradle clean;gradle assembleRelease;
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> sample release generated >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+adb root;
+sleep 2
+adb remount;
+sleep 2
+java -jar ../tools/signapk.jar ../tools/security/platform.x509.pem ../tools/security/platform.pk8 build/outputs/apk/sample-release-unsigned.apk build/outputs/apk/sample-release-signed.apk;
+echo ">>>> prepare install release signed sample apk >>>>"
+adb install -r build/outputs/apk/sample-release-signed.apk;
+cd ..;
