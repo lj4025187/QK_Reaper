b405e5ff0ed3f845e3582ebb796e56d92cc0f43c
zhangjigang <zhangjigang@qiku.com>
2017-08-22 14:45:34 +0800

    TFS_172435:zhangjigang_reaper_Remove unused codes
    修改点:　 删除没有用到的代码
    测试建议：ReaperSDK可以正常使用
    自测结果：测试通过
    
    Change-Id: Ib837b12b851864dcfc2e1e0dc79d302466772826
    Signed-off-by: zhangjigang <zhangjigang@qiku.com>

--- QK_Reaper/loader/src/main/java/com/fighter/loader/ReaperLoader.java
+++
@@ -1,8 +0,0 @@
-package com.fighter.loader;
-
-/**
- * Created by wxthon on 5/5/17.
- */
-
-public class ReaperLoader {
-}
--- QK_Reaper/reaper/src/main/java/com/fighter/protocol/Protocol.java
+++
@@ -1,8 +0,0 @@
-package com.fighter.protocol;
-
-/**
- * Created by wxthon on 5/5/17.
- */
-
-public class Protocol {
-}
