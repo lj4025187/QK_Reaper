676803a26970a454b55ffed8c14c78edf79735e1
liujia2 <liujia2@qiku.com>
2017-09-06 17:58:44 +0800

    TFS_172435:liujia_Update build bat file
    Change-Id: I662b6082a00b2f92994467873ac7a906a7e1f286
    Signed-off-by: liujia2 <liujia2@qiku.com>

--- Android_copy.bat
+++ Android_copy.bat
@@ -46,9 +46,9 @@ echo md bin
 rem set OUTPATH = %CODE_PATH%bin
 rem echo OUTPATH:%OUTPATH%
 
-copy /s /y /e /i  %CODE_PATH%\QK_Reaper\bin\reaper.aar %CODE_PATH%\bin\reaper.aar
-copy /s /y /e /i  %CODE_PATH%\QK_Reaper\bin\reaper.rr  %CODE_PATH%\bin\reaper.rr
-copy /s /y /e /i  %CODE_PATH%\QK_Reaper\bin\sample.apk %CODE_PATH%\bin\sample.apk
+copy /s /y /e /i  %CODE_PATH%\QK_Reaper\QK_Reaper\bin\reaper.aar %CODE_PATH%\bin\reaper.aar
+copy /s /y /e /i  %CODE_PATH%\QK_Reaper\QK_Reaper\bin\reaper.rr  %CODE_PATH%\bin\reaper.rr
+copy /s /y /e /i  %CODE_PATH%\QK_Reaper\QK_Reaper\bin\sample.apk %CODE_PATH%\bin\sample.apk
 
 echo ==============%PROJECT% copy finish!!!==============
 
